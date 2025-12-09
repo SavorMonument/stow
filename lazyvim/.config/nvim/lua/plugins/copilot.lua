51022f9a8756f56f114fbb605c4acc1e1721ba40
Alexandru Goia <savormonument@gmail.com>
2025-12-09 18:22:24 +0200

    Better copilot completion

--- lazyvim/.config/nvim/lua/plugins/copilot.lua
+++ lazyvim/.config/nvim/lua/plugins/copilot.lua
@@ -3,24 +3,9 @@ return {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     event = "InsertEnter",
-    config = function()
-      local copilot = require("copilot")
-
-      copilot.setup({
-        suggestion = { enabled = true, auto_trigger = false },
+    opts = {
+        suggestion = { enabled = true, auto_trigger = true, keymap = { accept = "\\<TAB>"} },
         panel = { enabled = false },
-      })
-
-      -- Map <leader><Tab> to trigger and accept suggestion
-      vim.keymap.set("i", "\\<Tab>", function()
-        local suggestion = require("copilot.suggestion")
-        if not suggestion.is_visible() then
-          suggestion.next()    -- generate a suggestion
-        end
-        if suggestion.is_visible() then
-          suggestion.accept()  -- accept it
-        end
-      end, { silent = true, noremap = true })
-    end,
+    },
   },
 }
